ed0055a82c09a976b2cbac769dd4d02417453ae8
Artiom <artiom.kreimer@gmail.com>
2013-03-18 23:45:24 -0400

    clean up and comments

--- rating.py
+++ rating.py
@@ -6,14 +6,18 @@ from google.appengine.ext import db
 from google.appengine.api import memcache
 from recipe_db import *
 
+
 def calcAverageRating(ratings):
+    """
+
+    :param ratings: list of ratings entities
+    :return: Average rating
+    """
     rate_dict = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
     average_rating = 0
     if ratings:
         counter = 0
         for rate in ratings:
-
-            #logging.error('Rating %s' % rate.rating)
             average_rating += int(rate_dict[rate.rating])
             counter += 1
         if counter > 0:
@@ -25,16 +29,18 @@ def calcAverageRating(ratings):
 
 
 def ratingsForRecipe(recipe_name, update=False):
-    '''
-    Memcache for ratings
-    '''
-    #key = recipe_name
+
+    """
+
+    :param recipe_name:
+    :param update:
+    :return:
+    """
     rating = memcache.get(recipe_name)
-    logging.error("_______________About to calculating average ratings___ %s" % update)
     if (rating is None) or (update is True):
-        #logging.error("_______________calculating average ratings_________________________")
         recipe = get_recipe_by_name(recipe_name)
-        ratings = recipe.ratings.fetch(1000)  #db.GqlQuery("SELECT * FROM Rating WHERE recipe_name = :1" , recipe_name)
+        ratings = recipe.ratings.fetch(1000)
+        #db.GqlQuery("SELECT * FROM Rating WHERE recipe_name = :1" , recipe_name)
         averageRating = calcAverageRating(ratings)
         memcache.set(recipe_name, averageRating)
     return rating
@@ -58,7 +64,6 @@ def update_rating(recipe_name, submitter, rating_value):
 
         for rate in ratings:
             if rate.submitter == submitter:
-                #logging.error("____________________upadting ratings")
                 rate.rating = rating_value
                 rate.put()
                 updated = True
@@ -85,9 +90,9 @@ def getRating(recipe_name):
 
 
 class Rating(db.Model):
-    submitter = db.StringProperty(required = True)
+    submitter = db.StringProperty(required=True)
     recipe = db.ReferenceProperty(Recipe, collection_name='ratings', required=True)
-    rating = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
+    rating = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
 
 
